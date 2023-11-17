f3e2007cb0b3e79453915f300fb8803f6d2c319e
Oleksiy Novykov <services@novykov.com>
2023-11-18 01:35:47 +0300

    clean up authentication

--- packages/auth/src/AuthenticationProvider.ts
+++ packages/auth/src/AuthenticationProvider.ts
@@ -1,11 +1,8 @@
 import { AuthContext } from "./AuthContext";
-import { ethers } from "ethers";
 import { AuthKitSignInData, Web3AuthModalPack } from "@safe-global/auth-kit";
 
 export class AuthenticationProvider {
     private authModalPack: Web3AuthModalPack;
-    private provider: ethers.providers.Web3Provider;
-    private signer: ethers.Signer;
 
     constructor() {
         AuthContext.init();
