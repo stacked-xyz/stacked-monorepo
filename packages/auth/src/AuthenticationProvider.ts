91b6dcfab6a867cc59c1e7bc2165264dc6ea4a2f
Oleksiy Novykov <services@novykov.com>
2023-11-18 01:34:05 +0300

    feat: signin signout

--- packages/auth/src/AuthenticationProvider.ts
+++ packages/auth/src/AuthenticationProvider.ts
@@ -1,5 +1,26 @@
 import { AuthContext } from "./AuthContext";
 import { ethers } from "ethers";
-import { Web3AuthModalPack } from "@safe-global/auth-kit";
+import { AuthKitSignInData, Web3AuthModalPack } from "@safe-global/auth-kit";
 
-export class AuthenticationProvider {}
+export class AuthenticationProvider {
+    private authModalPack: Web3AuthModalPack;
+    private provider: ethers.providers.Web3Provider;
+    private signer: ethers.Signer;
+
+    constructor() {
+        AuthContext.init();
+        this.authModalPack = AuthContext.getInstance().getAuthModalPack();
+    }
+
+    public async signIn(): Promise<AuthKitSignInData> {
+        // Calling signIn method from the Web3AuthModalPack instance
+        const authKitSignData = await this.authModalPack.signIn();
+        return authKitSignData;
+
+    }
+
+    public async signOut(): Promise<void> {
+        // Calling signOut method from the Web3AuthModalPack instance
+        await this.authModalPack.signOut();
+    }
+}
