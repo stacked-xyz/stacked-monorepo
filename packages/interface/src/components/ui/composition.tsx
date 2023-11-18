ec4a414c851534beab04f1fa64fda4fdefd3a2f4
denviljclarke <60730266+denviljclarke@users.noreply.github.com>
2023-11-18 21:16:11 +0300

    feat: update token pie chart

--- packages/interface/src/components/ui/composition.tsx
+++ packages/interface/src/components/ui/composition.tsx
@@ -1,52 +1,76 @@
 "use client";
 
-import { Pie, PieChart, ResponsiveContainer } from "recharts";
-import { DialogOverlay } from "./dialog";
+import { Cell, Pie, PieChart, ResponsiveContainer } from "recharts";
 
-const data02 = [
-  {
-    name: "ETH",
-    value: 2400,
-  },
-  {
-    name: "BTC",
-    value: 4567,
-  },
-];
+import { Allocations } from "../update-allocation";
 
-export function Composition({}) {
+export function Composition({ allocations }: { allocations: Allocations }) {
+  const data = Object.keys(allocations).map((key) => {
+    return { name: key, value: allocations[key] };
+  });
+
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+    name,
+  }: {
+    cx: number;
+    cy: number;
+    midAngle: number;
+    innerRadius: number;
+    outerRadius: number;
+    percent: number;
+    name: string;
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos((-midAngle * Math.PI) / 180);
+    const y = cy + radius * Math.sin((-midAngle * Math.PI) / 180);
+
+    return (
+      <text
+        x={x}
+        y={y}
+        fill="black"
+        textAnchor={x > cx ? "start" : "end"}
+        dominantBaseline="central"
+      >
+        {`${(percent * 100).toFixed(0)}% ${name}`}
+      </text>
+    );
+  };
+
+  const neonColors = [
+    "#ccff00",
+    "#ff00ff",
+    "#33ccff",
+    "#ff0066",
+    "#66ff66" /* more colors as needed */,
+  ];
+
   return (
     <ResponsiveContainer width="100%" height={350}>
       <PieChart width={730} height={250}>
         <Pie
-          label
-          data={data02}
+          label={renderCustomizedLabel}
+          data={data}
           dataKey="value"
           nameKey="name"
-          //   cx="50%"
-          //   cy="50%"
-          //   outerRadius={50}
+          labelLine={false}
           fill="#adfa1d"
-        />
+          animationDuration={500}
+        >
+          {data.map((entry, index) => (
+            <Cell
+              key={`cell-${index}`}
+              fill={neonColors[index % neonColors.length]}
+            />
+          ))}
+        </Pie>
       </PieChart>
-
-      {/* <BarChart data={data}>
-        <XAxis
-          dataKey="name"
-          stroke="#888888"
-          fontSize={12}
-          tickLine={false}
-          axisLine={false}
-        />
-        <YAxis
-          stroke="#888888"
-          fontSize={12}
-          tickLine={false}
-          axisLine={false}
-          tickFormatter={(value: number) => `$${value}`}
-        />
-        <Bar dataKey="total" fill="#adfa1d" radius={[4, 4, 0, 0]} />
-      </BarChart> */}
     </ResponsiveContainer>
   );
 }
