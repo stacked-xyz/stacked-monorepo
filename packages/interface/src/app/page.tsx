1bc770992fbd897768ced5263204528cac26e9e8
boqdan <bogdan@bowd.io>
2023-11-18 23:04:22 +0300

    wngmi

--- packages/interface/src/app/page.tsx
+++ packages/interface/src/app/page.tsx
@@ -12,6 +12,7 @@ import { OrderHistory } from "@/components/order-history";
 import { getAllocationObject } from "@/lib/utils";
 import { useComposition } from "@/store/allocationsContext";
 import { useAccountAbstraction } from "@/store/accountAbstractionContext";
+import { MainNav } from "@/components/ui/main-nav";
 
 export default function Home() {
   const { isAuthenticated } = useAccountAbstraction();
@@ -33,7 +34,7 @@ export default function Home() {
         <div className="border-b">
           <div className="flex h-16 items-center px-4">
             {/* <TeamSwitcher /> */}
-            {/* <MainNav className="mx-6" /> */}
+            <MainNav className="mx-6" />
             <div className="ml-auto flex items-center space-x-4">
               {/* <Search />
                 <UserNav /> */}
