eb2cccbe23de423574220cc7a5abde8268f5fa6f
Oleksiy Novykov <services@novykov.com>
2023-11-19 04:59:25 +0300

    updated landing

--- packages/interface/src/app/login/page.tsx
+++ packages/interface/src/app/login/page.tsx
@@ -19,28 +19,19 @@ export default function AuthenticationPage() {
             <div className="relative flex-col hidden h-full p-10 text-white bg-muted dark:border-r lg:flex">
                <div className="absolute inset-0 bg-zinc-900" />
                <div className="relative z-20 flex items-center text-lg font-medium">
-                  <svg
-                     xmlns="http://www.w3.org/2000/svg"
-                     viewBox="0 0 24 24"
-                     fill="none"
-                     stroke="currentColor"
-                     strokeWidth="2"
-                     strokeLinecap="round"
-                     strokeLinejoin="round"
-                     className="w-6 h-6 mr-2"
-                  >
-                     <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-                  </svg>
+                  <img
+                     src="/stacked-logo.svg"
+                     alt="Stacked logo"
+                     className="h-8 mr-2"
+                  />
                   Staked
                </div>
                <div className="relative z-20 mt-auto">
                   <blockquote className="space-y-2">
                      <p className="text-lg">
-                        &ldquo;This library has saved me countless hours of work and
-                        helped me deliver stunning designs to my clients faster than
-                        ever before.&rdquo;
+                        &ldquo;The easiet way to build crypto portfolio for an average human.&rdquo;
                      </p>
-                     <footer className="text-sm">Sofia Davis</footer>
+                     <footer className="text-sm">With Love. Mento Labs.</footer>
                   </blockquote>
                </div>
             </div>
