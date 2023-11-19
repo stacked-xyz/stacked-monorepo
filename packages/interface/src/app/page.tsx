402ee301a36df20c40cc8c28e3d415b39b4a7024
Oleksiy Novykov <services@novykov.com>
2023-11-19 04:35:28 +0300

    removed smaller balance

--- packages/interface/src/app/page.tsx
+++ packages/interface/src/app/page.tsx
@@ -85,7 +85,7 @@ export default function Home() {
             <div className="flex-1 p-8 pt-6 space-y-4">
                <div className="flex items-center justify-between space-y-2">
                   <h2 className="text-3xl font-bold tracking-tight">
-                     Dashboard
+                     Topup your balance. Choose assets and allocations. See how your protfolio grows.
                   </h2>
                </div>
                <div className="grid grid-cols-1 gap-4 md:grid-cols-4">
@@ -111,9 +111,6 @@ export default function Home() {
                               <div className="text-2xl font-bold">
                                  $45,231.89 USD
                               </div>
-                              <p className="text-xs text-muted-foreground">
-                                 $45,231.89 USD
-                              </p>
                            </div>
 
                            {/* <BalanceList
